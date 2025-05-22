c1b736bc31bbc5f39d58883f58851c53bdc0e9a9
BansalSwayam <swayamb1281@gmail.com>
2025-05-23 04:30:38 +0530

    walletR.js

--- routes/walletR.js
+++ routes/walletR.js
@@ -5,7 +5,18 @@ import { deposit, withdraw, transfer, history } from '../controllers/walletC.js'
 
 const router = express.Router();
 router.post('/deposit', auth, deposit);
-router.post('/withdraw', auth, fraudCheck, withdraw);
+// router.post('/withdraw', auth, fraudCheck, withdraw);
+router.post(
+  '/withdraw',
+  auth,
+  (req, res, next) => {
+    req.body.type = 'withdraw'; 
+    next();
+  },
+  fraudCheck,
+  withdraw
+);
+
 router.post('/transfer/c/:to', auth, fraudCheck, transfer);
 router.get('/history', auth, history);
 export default router;
